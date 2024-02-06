558de11854c5d1150e0b6fc102bccb90936e67fa
Dvôrakcs <Mhsf15@hotmail.com>
2024-02-06 17:18:58 -0300

    adicionando authorize na controller

--- learnmvcreactapp.client/src/services/ProdutosServices.jsx
+++ learnmvcreactapp.client/src/services/ProdutosServices.jsx
@@ -1,12 +1,22 @@
 import axios from "axios"
+import Cookies from "universal-cookie";
 class ProdutosServices {
   #ApiBaseUrl = "https://localhost:7236/api/Produtos/"
 
 
   async get(){
     try {
-      const response = await axios.get(this.#ApiBaseUrl)
-      return response;
+      const cookies = new Cookies();
+      const token = cookies.get('token');
+      if(token != null){
+        const response = await axios.get(this.#ApiBaseUrl,{
+          headers: {
+            Authorization: `Bearer ${token}` // Adiciona o token como um cabeçalho de autorização Bearer
+          }
+        })
+        return response;
+      }
+      
     } catch (error) {
       console.error(error)
     }
