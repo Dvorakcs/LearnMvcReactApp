450ddcec2eae4608c3aa320fe734834c934cd090
Dvôrakcs <Mhsf15@hotmail.com>
2024-02-06 17:13:48 -0300

    #10  - FINALIZANDO

--- LearnMvcReactApp.Server/Controllers/UsuariosController.cs
+++ LearnMvcReactApp.Server/Controllers/UsuariosController.cs
@@ -1,6 +1,7 @@
 ﻿using LearnMvcReactApp.Server.Models;
 using LearnMvcReactApp.Server.Repositories;
 using LearnMvcReactApp.Server.Repositories.IRepositories;
+using LearnMvcReactApp.Server.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LearnMvcReactApp.Server.Controllers
@@ -9,11 +10,13 @@ namespace LearnMvcReactApp.Server.Controllers
     public class UsuariosController : GenericController<Usuarios, IUsuariosRepository>
     {
         private readonly IUsuariosRepository _usuarioRepository;
+        private readonly ITokenService _tokenService;
 
-        public UsuariosController(IUsuariosRepository usuariosRepository)
+        public UsuariosController(IUsuariosRepository usuariosRepository, ITokenService tokenService)
             : base(usuariosRepository)
         {
             _usuarioRepository = usuariosRepository;
+            _tokenService = tokenService;
         }
         protected override int GetEntityId(Usuarios entity)
         {
@@ -30,8 +33,17 @@ namespace LearnMvcReactApp.Server.Controllers
         public async Task<ActionResult<Usuarios>> Login([FromBody] Usuarios usuario)
         {
             var usuarioDb = await _usuarioRepository.GetByName(usuario.Nome);
+            if(usuarioDb is null)
+            {
+                return NotFound();
+            }
+            var Token = await _tokenService.GetTokenAsync(usuarioDb);
 
-            return Ok(usuarioDb);
+            return Ok(new
+            {
+                token=Token,
+                usuario = usuarioDb,
+            });
         }
     }
 }
--- learnmvcreactapp.client/src/App.jsx
+++ learnmvcreactapp.client/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import Produtos from './pages/Produtos';
+import Usuarios from './pages/Usuarios';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth';
 class App extends React.Component {
     render() {
         return (
@@ -9,6 +11,8 @@ class App extends React.Component {
             <Routes>
                 <Route path='/'>
                     <Route path='/produtos' element={<Produtos/>}/>
+                    <Route path='/auth' element={<Auth/>}/>
+                    <Route path='/usuarios' element={<Usuarios/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
